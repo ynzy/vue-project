c501d3a4fafa01b01454417564d86de3182fa718
ynzy <a758922096@gmail.com>
2019-02-27 16:27:30 +0800

    引入头部组件

--- vue-music-app/src/App.vue
+++ vue-music-app/src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <div id="app">
-
-    <router-view/>
+    <v-header></v-header>
+    <!-- <router-view/> -->
   </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue"
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    "v-header": Header
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+
 </style>
---
+++ vue-music-app/src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="header">
+        <div class="s-top">
+            <div class="logo">
+                我的音乐
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+.s-top
+  height 64px
+  background-color #d43c33
+  .logo
+      width 100%
+      height 64px
+      line-height 64px
+      text-align center 
+      font-family '微软雅黑'
+      font-size 20px
+      color #fff
+      background url('../../static/img/music_logo.png') left center no-repeat
+      background-size 18%
+
+</style>
