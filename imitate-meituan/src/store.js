3000bd051a505bb9bb8c88dabbbf7a8edd5276d1
ynzy <a758922096@gmail.com>
2019-03-06 15:06:28 +0800

    no message

--- imitate-meituan/src/store.js
+++ imitate-meituan/src/store.js
@@ -3,11 +3,24 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-    state: {
-        
-    },
-    mutations: {
+var state = {
+  shop: {
+    num: 1,
+    list: [],
+    page: 0
+  }
+}
 
-    }
+const mutations = {
+  setshoplist(state, data) {
+    state.shop.list = state.shop.list.concat(data)
+  },
+  nextpage(state){
+    state.shop.page++;
+  }
+}
+
+export default new Vuex.Store({
+  state,
+  mutations
 })
